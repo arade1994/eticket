5e366c7b5c8b8741889cd2574bd59fbdd7816731
Ante Rade <anterade99@gmail.com>
2025-04-08 00:00:11 +0200

    test:fixed some tests and config

--- services/client/vitest-setup.ts
+++ services/client/vitest-setup.ts
@@ -2,10 +2,6 @@ import { afterAll, afterEach, vi } from "vitest";
 
 import { cleanup, configure } from "@testing-library/react";
 
-vi.useFakeTimers();
-
-process.env.TZ = "Europe/Belgrade";
-
 vi.stubGlobal("Intl", {
   DateTimeFormat: () => ({
     resolvedOptions: () => ({
