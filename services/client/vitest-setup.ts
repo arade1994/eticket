5fab957f42e01e5ee197138a6bf358a0233d8ab9
Ante Rade <anterade99@gmail.com>
2025-04-07 23:51:48 +0200

    fix: added timezones support with dayjs

--- services/client/vitest-setup.ts
+++ services/client/vitest-setup.ts
@@ -1,10 +1,26 @@
-import { afterEach, vi } from "vitest";
+import { afterAll, afterEach, vi } from "vitest";
 
 import { cleanup, configure } from "@testing-library/react";
+
+vi.useFakeTimers();
+
+process.env.TZ = "Europe/Belgrade";
+
+vi.stubGlobal("Intl", {
+  DateTimeFormat: () => ({
+    resolvedOptions: () => ({
+      timeZone: "Europe/Belgrade",
+    }),
+  }),
+});
 
 afterEach(() => {
   cleanup();
   vi.clearAllMocks();
+});
+
+afterAll(() => {
+  vi.useRealTimers();
 });
 
 configure({ testIdAttribute: "id" });
