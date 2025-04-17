a7ad98068bc8ab25afb1824ef2fed020d7067d76
Ante Rade <anterade99@gmail.com>
2025-04-17 19:51:27 +0200

    fix: experimental console.log

--- services/auth/src/routes/__test__/currentUser.test.ts
+++ services/auth/src/routes/__test__/currentUser.test.ts
@@ -6,6 +6,8 @@ describe("Api which gives us information about currently loged in user", () => {
   test("Returns some data about authenticated user", async () => {
     const cookie = await globalThis.signup();
 
+    console.log({ cookie });
+
     const response = await request(app)
       .get("/api/users/currentuser")
       .set("Cookie", cookie)
