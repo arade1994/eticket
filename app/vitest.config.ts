ffc4d546101752bb3d17a679ce6631e4f28232cc
Ante Rade <anterade99@gmail.com>
2025-07-03 22:59:28 +0200

    fix: added summary for tests report

--- app/vitest.config.ts
+++ app/vitest.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     setupFiles: "./vitest-setup.ts",
     coverage: {
       enabled: true,
-      reporter: ["text", "json", "lcov"],
+      reporter: ["text", "text-summary", "json", "lcov"],
       thresholds: {
         lines: 80,
         functions: 80,
